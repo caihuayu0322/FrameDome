1a941eb42afc3e3d113872d06f697bf8a094e879
caicai <837522223@qq.com>
2018-04-13 00:59:03 +0800

    init

---
+++ apacheHttp/Http/src/test/java/com/caicai/http/TestHttp.java
@@ -0,0 +1,12 @@
+package com.caicai.http;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
+public class TestHttp {
+
+
+    @Test
+    public void testHttp(){
+    }
+}
